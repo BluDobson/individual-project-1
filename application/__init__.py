4732b22e5b1858647c7626805f6aebdb5d66be4d
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-23 08:25:44 +0000

    updated init.py

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('d_uri')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('duri')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = os.getenv('s_key')
+app.config["SECRET_KEY"] = os.getenv('skey')
 db = SQLAlchemy(app)
 
 from application import routes
