51a4b4655a32134e1ecbd49ab9853f3e8d25ee50
Bradley Dobson <84865402+BluDobson@users.noreply.github.com>
2021-06-23 12:18:58 +0100

    Developed Add Route for Ideas (#2)
    * updated ideas.html and routes.py
    
    * added route for adding ideas
    
    * updated routes.py
    
    * updated routes.py
    
    * updated routes.py
    
    * updated routes.py
    
    * updated layout.html
    
    * updated layout.html
    
    * updated layout.html
    
    * updated routes.py and layout.html
    
    * updated ideas.html
    
    * updated ideas.html
    
    * updated ideas.html
    
    * updated routes.py
    
    * updated ideas.html

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,36 @@
 from application import app, db
 from application.models import Ideas, Tags
-from flask import Flask, render_template
+from application.forms import ideaForm
+from flask import Flask, render_template, request
+
+def get_num(string):
+    ls = ""
+    for char in string:
+        if char.isdigit():
+            ls += char
+    return int(ls)
 
 @app.route('/')
 def ideas_home():
     all_ideas = Ideas.query.all()
     all_tags = Tags.query.all()
     return render_template('ideas.html', ideas = all_ideas, tags = all_tags)
+
+@app.route('/add/idea', methods=['GET', 'POST'])
+def add_idea():
+    form = ideaForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            title = form.title.data
+            description = form.description.data
+            tag_id = str(form.tag_id.data)
+            tag_id = get_num(tag_id)
+            name = form.name.data
+            new_idea = Ideas(title=title, description=description, tag_id=tag_id, name=name)
+            db.session.add(new_idea)
+            db.session.commit()
+            return f'Added {title} to ideas!'
+        else:
+            return render_template('idea_entry.html', form=form, title="", description="", tag_id="", name="")
+    else:
+        return render_template('idea_entry.html', form=form)
