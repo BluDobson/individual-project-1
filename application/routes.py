5de72c00fbd82033bf9c015f86d1b1236234821f
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-23 09:09:49 +0000

    updated ideas.html and routes.py

--- application/routes.py
+++ application/routes.py
@@ -5,5 +5,5 @@ from flask import Flask, render_template
 @app.route('/')
 def ideas_home():
     all_ideas = Ideas.query.all()
-    all_tags = Tags.query.all()
-    return render_template('ideas.html', ideas = all_ideas, tags = all_tags)
+
+    return render_template('ideas.html', ideas = all_ideas)
