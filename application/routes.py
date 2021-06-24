48b7a60f44644a955a8c450811a64c5fa031d0d4
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-24 11:42:32 +0000

    updated add route

--- application/routes.py
+++ application/routes.py
@@ -32,11 +32,7 @@ def add_idea():
             db.session.commit()
             return redirect(url_for('ideas_home'))
         else:
-            error = next(iter(form.title.errors), None)
-            error = next(iter(form.description.errors), None)
-            error = next(iter(form.tag_id.errors), None)
-            error = next(iter(form.name.errors), None)
-            return render_template('idea_entry.html', form=form, error=error, title="", description="", tag_id="", name="")
+            return render_template('idea_entry.html', form=form, title="", description="", tag_id="", name="")
     else:
         return render_template('idea_entry.html', form=form)
 
