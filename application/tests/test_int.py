2434edb3ba27e72a0fc410deb2c37a8f15992c1a
Bradley Dobson <84865402+BluDobson@users.noreply.github.com>
2021-06-24 11:08:51 +0100

    Unit and Integration Testing (#5)
    * updated ideas.html and routes.py
    
    * added route for adding ideas
    
    * updated routes.py
    
    * updated routes.py
    
    * updated routes.py
    
    * updated routes.py
    
    * updated layout.html
    
    * updated layout.html
    
    * updated layout.html
    
    * updated routes.py and layout.html
    
    * updated ideas.html
    
    * updated ideas.html
    
    * updated ideas.html
    
    * updated routes.py
    
    * updated ideas.html
    
    * added update idea function
    
    * added update idea function
    
    * updated routes.py
    
    * updated routes.py
    
    * updated forms.py
    
    * updated routes.py
    
    * updated routes.py
    
    * added delete route
    
    * updated routes.py
    
    * created unit-testing suite
    
    * added test for add route
    
    * more unit tests
    
    * added integration test
    
    * added some values in create to show off functionality

---
+++ application/tests/test_int.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from flask import url_for
+from flask_testing import LiveServerTestCase
+from urllib.request import urlopen
+from application import app, db
+from application.models import Ideas, Tags
+
+class TestBase(LiveServerTestCase):
+    TEST_PORT = 5050
+
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///test.db", LIVESERVER_PORT=self.TEST_PORT, DEBUG=True, TESTING=True)
+        return app
+
+    def setUp(self):
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=chrome_options)
+        db.create_all()
+        t1 = Tags(name='Landscape')
+        t2 = Tags(name='Portrait')
+        db.session.add(t1)
+        db.session.add(t2)
+        db.session.commit()
+        self.driver.get(f'http://localhost:{self.TEST_PORT}')
+
+    def tearDown(self):
+        self.driver.quit()
+        db.drop_all()
+
+    def test_server_running(self):
+        response = urlopen(f'http://localhost:{self.TEST_PORT}')
+        self.assertEqual(response.code, 200)
+
+class BasicTests(TestBase):
+    def home_test(self):
+        first_tag = self.driver.find_element_by_xpath('/html/body/h6[1]')
+        self.assertEqual(first_tag, '1 Landscape')
+
+    def add_test(self):
+        self.driver.find_element_by_xpath('/html/body/a[2]').click()
+        assert self.driver.current_url == f'http://localhost:{self.TEST_PORT}/add/idea' 
+        input_box = self.driver.find_element_by_xpath('//*[@id="title"]')
+        input_box.send_keys('Pilot')
+        input_box = self.driver.find_element_by_xpath('//*[@id="description"]')
+        input_box.send_keys('See if the pilot would take your camera for some shots inside the cockpit!')
+        input_tag_id = driver.find_element_by_xpath('//*[@id="tag_id"]/option[2]').click()
+        input_box = self.driver.find_element_by_xpath('//*[@id="name"]')
+        input_box.send_keys('Peter')
+        self.driver.find_element_by_xpath('//*[@id="submit"]')
+        assert self.driver.current_url == f'http://localhost:{self.TEST_PORT}'
+        first_entry_title = self.driver.find_element_by_xpath('/html/body/p[1]')
+        self.assertIn(first_entry_title, 'Pilot') 
